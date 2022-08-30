2c40dc825578b8170034a35688e40fdae9421b9b
Sakthivel <sakthivelvengatasamy@gmail.com>
2022-08-30 17:51:58 +0530

    Deploy to github

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()]
+  plugins: [svgr(), react()],
+  build: {
+    outDir: './docs'
+  }
 })
